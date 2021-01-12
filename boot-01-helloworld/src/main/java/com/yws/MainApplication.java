036b4d93f1075ecd22159dc1d36281012f0fc8be
yws <yws528819@163.com>
2021-01-12 08:08:26 +0800

    @Configuration 详解

--- boot-01-helloworld/src/main/java/com/yws/MainApplication.java
+++ boot-01-helloworld/src/main/java/com/yws/MainApplication.java
@@ -1,7 +1,11 @@
 package com.yws;
 
+import com.yws.bean.Pet;
+import com.yws.bean.User;
+import com.yws.config.MyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @SpringBootApplication ：这是一个SpringBoot应用
@@ -9,6 +13,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        //1.返回我们的IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+
+        //2.查看容器里面的组件
+        String[] beanDefinitionNames = run.getBeanDefinitionNames();
+
+        for(String name : beanDefinitionNames) {
+            System.out.println(name);
+        }
+
+        //3.从容器中获取组件
+        Runnable runnable01 = run.getBean("runnable", Runnable.class);
+        Runnable runnable02 = run.getBean("runnable",  Runnable.class);
+        System.out.println("组件：" + (runnable01 == runnable02));
+
+        //4.com.yws.config.MyConfig$$EnhancerBySpringCGLIB$$3e31a857@6622a690
+        MyConfig bean = run.getBean(MyConfig.class);
+        System.out.println(bean);
+
+        //如果 @Configuration(proxyBean=true) 代理对象调用方法。SpringBoot总会检查这个组件是否在容器里。
+        //保存组件单实例
+        User user01 = bean.user01();
+        User user02 = bean.user01();
+        System.out.println(user01 == user02);
+
+
+        //组件依赖 与 @Configuration(proxyBeanMethods = false) 作用
+        User user = run.getBean("user01", User.class);
+        Pet pet = run.getBean("pet01", Pet.class);
+        System.out.println("用户的宠物是不是同一只" + (user.getPet() == pet));
+
+
     }
 }
---
+++ boot-01-helloworld/src/main/java/com/yws/config/MyConfig.java
@@ -0,0 +1,45 @@
+package com.yws.config;
+
+import com.yws.bean.Pet;
+import com.yws.bean.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 1.配置类使用 @Bean 标注在方法上给容器注册组件，默认也是单实例的。
+ * 2.配置类本身也是组件
+ * 3.proxyBeanMethods：代理bean的方法
+ *      Full、Lite
+ */
+@Configuration(proxyBeanMethods = false)  //告诉SpringBoot 这是一个配置类 == 配置文件
+public class MyConfig {
+
+    /**
+     * 外部无论对注册类中的这个组件注册方法调用多少次，获取的都是之前注册容器中的单实例对象
+     * @return
+     */
+    @Bean //给容器中添加组件。以方法名作为组件的id。返回类型就是组件的类型。返回的值，就是组件在容器中的实例。
+    public Runnable runnable() {
+        return ()-> {};
+    }
+
+    @Bean
+    public User user01() {
+        User user = new User("张三", 18);
+        //user组件依赖了Pet组件
+        user.setPet(pet01());
+        return user;
+    }
+
+    @Bean
+    public Pet pet01() {
+        return new Pet("tom");
+    }
+
+
+//    public static void main(String[] args) {
+//        Runnable run1 = ()-> {};
+//        Runnable run2 = ()-> {};
+//        System.out.println(run1 == run2);
+//    }
+}
