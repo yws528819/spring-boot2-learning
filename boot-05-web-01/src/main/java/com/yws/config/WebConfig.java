7f4c21a9a91cff0af7712a5027d54a0ffe9efe98
yws <yws528819@163.com>
2021-01-22 08:05:50 +0800

    矩阵变量测试

--- boot-05-web-01/src/main/java/com/yws/config/WebConfig.java
+++ boot-05-web-01/src/main/java/com/yws/config/WebConfig.java
@@ -3,9 +3,12 @@ package com.yws.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
-public class WebConfig {
+public class WebConfig /*implements WebMvcConfigurer*/ {
 
     /**
      * 自定义rest请求的隐藏域的参数名
@@ -16,5 +19,24 @@ public class WebConfig {
         HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
         hiddenHttpMethodFilter.setMethodParam("_m");
         return hiddenHttpMethodFilter;
+    }
+
+//    @Override
+//    public void configurePathMatch(PathMatchConfigurer configurer) {
+//        UrlPathHelper urlPathHelper = new UrlPathHelper();
+//        urlPathHelper.setRemoveSemicolonContent(false);
+//        configurer.setUrlPathHelper(urlPathHelper);
+//    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void configurePathMatch(PathMatchConfigurer configurer) {
+                UrlPathHelper urlPathHelper = new UrlPathHelper();
+                urlPathHelper.setRemoveSemicolonContent(false);
+                configurer.setUrlPathHelper(urlPathHelper);
+            }
+        };
     }
 }
--- boot-05-web-01/src/main/java/com/yws/controller/ParameterTestController.java
+++ boot-05-web-01/src/main/java/com/yws/controller/ParameterTestController.java
@@ -12,7 +12,7 @@ public class ParameterTestController {
 
     @GetMapping("/car/{id}/owner/{username}")
     public Map<String, Object> getCar(
-            @PathVariable("id") String id,
+            @PathVariable("id") Integer id,
             @PathVariable("username") String username,
             @PathVariable Map<String, String> pv,
 
@@ -43,11 +43,49 @@ public class ParameterTestController {
         return map;
     }
 
-    @PostMapping(value = "/save", produces = "application/json;charset=utf-8")
+    @PostMapping(value = "/save")
     public Map<String, Object> save(@RequestBody String bodyContent) {
         Map<String, Object> map = new HashMap<>();
         map.put("content", bodyContent);
         System.out.println(bodyContent);
+        return map;
+    }
+
+
+    //1.语法：请求路径：/cars/sell;low=34;brand=byd,audi,yd
+    //2.SpringBoot默认是禁用了矩阵变量的功能
+    //      手动开启：原理。对于路径的处理。UrlPathHelper进行解析。
+    //              removeSemicolonContent（移除分号内容）支持矩阵变量
+    //3.矩阵变量必须有url路径变量才能被解析
+
+    // /cars/sell;low=34;brand=byd,audi,yd
+
+    @GetMapping("/cars/{path}")
+    public Map<String, Object> carSell(
+            @MatrixVariable("low") Integer low,
+            @MatrixVariable("brand") List<String> brand,
+            @PathVariable("path") String path
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("low", low);
+        map.put("brand", brand);
+        map.put("path", path);
+
+        return map;
+    }
+
+    // /boss/1;age=30/2;age=24
+    @GetMapping("/boss/{bossId}/{empId}")
+    public Map<String, Object> boss(
+            @MatrixVariable(value = "age", pathVar = "bossId") Integer bossAge,
+            @MatrixVariable(value = "age", pathVar = "empId") Integer empAge,
+            @PathVariable("bossId") String bossId
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("bossAge", bossAge);
+        map.put("empAge", empAge);
+        map.put("bossId", bossId);
+
         return map;
     }
 }
