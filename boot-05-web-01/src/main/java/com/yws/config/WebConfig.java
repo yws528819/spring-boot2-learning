861853e373a96ba4694b8ed62bb4e0d00ca3854b
yws <yws528819@163.com>
2021-02-01 08:07:12 +0800

    自定义内容协商策略

--- boot-05-web-01/src/main/java/com/yws/config/WebConfig.java
+++ boot-05-web-01/src/main/java/com/yws/config/WebConfig.java
@@ -6,14 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.StringUtils;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.accept.ParameterContentNegotiationStrategy;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
 
-import java.util.List;
+import java.util.*;
 
 @Configuration
 public class WebConfig /*implements WebMvcConfigurer*/ {
@@ -66,6 +70,25 @@ public class WebConfig /*implements WebMvcConfigurer*/ {
             @Override
             public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
                 converters.add(new YwsMessageConverter());
+            }
+
+            /**
+             * 自定义内容协商策略
+             * @param configurer
+             */
+            @Override
+            public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+                Map<String, MediaType> mediaTypes = new HashMap<>();
+                mediaTypes.put("json", MediaType.APPLICATION_JSON);
+                mediaTypes.put("xml", MediaType.APPLICATION_XML);
+                mediaTypes.put("yws", MediaType.parseMediaType("application/x-yws"));
+                //指定支持解析哪些参数对应的哪些媒体类型
+                ParameterContentNegotiationStrategy parameterStrategy = new ParameterContentNegotiationStrategy(mediaTypes);
+                //parameterStrategy.setParameterName("ff");
+
+                HeaderContentNegotiationStrategy headStrategy = new HeaderContentNegotiationStrategy();
+
+                configurer.strategies(Arrays.asList(parameterStrategy, headStrategy));
             }
         };
     }
