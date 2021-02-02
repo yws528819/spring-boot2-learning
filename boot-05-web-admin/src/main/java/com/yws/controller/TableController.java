04604150657ed6607b1f48771f791207ea0be4c2
yws <yws528819@163.com>
2021-02-02 13:46:09 +0800

    公共页面完善

--- boot-05-web-admin/src/main/java/com/yws/controller/TableController.java
+++ boot-05-web-admin/src/main/java/com/yws/controller/TableController.java
@@ -1,7 +1,12 @@
 package com.yws.controller;
 
+import com.yws.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TableController {
@@ -14,7 +19,15 @@ public class TableController {
 
 
     @GetMapping("/dynamic_table")
-    public String dynamicTable() {
+    public String dynamicTable(Model model) {
+
+        List<User> users = Arrays.asList(
+                new User("zhangsan", "123"),
+                new User("lisi", "345"),
+                new User("wangwu", "568"),
+                new User("zhaoliu", "17723")
+        );
+        model.addAttribute("users", users);
 
         return "table/dynamic_table";
     }
