bef7bb4fe55c51f426d7afa3aba25e36793a47c7
yws <yws528819@163.com>
2021-02-25 13:45:33 +0800

    整合数据源

--- boot-05-web-admin/src/test/java/com/yws/Boot05WebAdminApplicationTests.java
+++ boot-05-web-admin/src/test/java/com/yws/Boot05WebAdminApplicationTests.java
@@ -1,13 +1,23 @@
 package com.yws;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class Boot05WebAdminApplicationTests {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+        //jdbcTemplate.queryForObject("select * from department",);
+        Long aLong = jdbcTemplate.queryForObject("select count(1) from department", Long.class);
+        log.info("记录总数：{}", aLong);
     }
 
 }
