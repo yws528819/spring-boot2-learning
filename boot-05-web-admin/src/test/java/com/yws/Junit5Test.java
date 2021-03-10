e7f6a056ed40dbce3c9df4545f710b788bb419bd
yws <yws528819@163.com>
2021-03-10 13:44:21 +0800

    单元测试2

--- boot-05-web-admin/src/test/java/com/yws/Junit5Test.java
+++ boot-05-web-admin/src/test/java/com/yws/Junit5Test.java
@@ -6,8 +6,70 @@ import org.springframework.test.annotation.Repeat;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 //@SpringBootTest
 public class Junit5Test {
+
+
+    /**
+     * 断言：简单断言失败，后面的代码不会执行
+     */
+    @DisplayName("测试简单断言")
+    @Test
+    void testSimpleAssertions() {
+        int cal = cal(2, 3);
+        assertEquals(5, cal, "业务逻辑计算失败");
+
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        assertNotSame(obj1, obj2, "两个对象不一样");
+    }
+
+    int cal(int i, int j) {
+       return i + j;
+    }
+
+
+    @DisplayName("array assertion")
+    @Test
+    void array() {
+        assertArrayEquals(new int[] {2,1}, new int[] {2, 1}, "数组内容不相等");
+    }
+
+
+    @DisplayName("组合断言")
+    @Test
+    void all() {
+        /**
+         * 所有断言全部需要成功
+         */
+        assertAll("test",
+                ()-> {
+                    assertTrue( true && true, "结果不为TRUE");
+                },
+                () -> {
+                    assertEquals("1", "1", "结果不为1");
+                });
+    }
+
+
+    @DisplayName("异常断言")
+    @Test
+    void testException() {
+        assertThrows(ArithmeticException.class, ()-> { int i = 10/0; }, "业务逻辑居然正常运行？");
+    }
+
+
+    @DisplayName("快速失败")
+    @Test
+    void testFail() {
+        if (1 == 2) {
+            fail("测试失败");
+        }
+    }
+
+
 
     @DisplayName("测试DisplayName注解")
     @Test
