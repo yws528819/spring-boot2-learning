6776209afdd2f9200a08f7033359b7d3f7f843eb
yws <yws528819@163.com>
2021-04-06 07:59:38 +0800

    自定义监听事件

---
+++ boot-09-features-profile/src/main/java/com/yws2/listener/MyApplicationContextInitializer.java
@@ -0,0 +1,11 @@
+package com.yws2.listener;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class MyApplicationContextInitializer implements ApplicationContextInitializer {
+    @Override
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+        System.out.println("MyApplicationContextInitializer.initialize...");
+    }
+}
---
+++ boot-09-features-profile/src/main/java/com/yws2/listener/MyApplicationListener.java
@@ -0,0 +1,11 @@
+package com.yws2.listener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public class MyApplicationListener implements ApplicationListener {
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+        System.out.println("MyApplicationListener.onApplicationEvent...");
+    }
+}
